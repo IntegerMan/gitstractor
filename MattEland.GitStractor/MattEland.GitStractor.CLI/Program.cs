fab4e3a89734399beb2966c9cfd27645ce397d1b
Matt Eland <matt.eland@gmail.com>
2023-03-25 16:04:55 -0400

    Adding quick prototype.

---
+++ MattEland.GitStractor/MattEland.GitStractor.CLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using MattEland.GitStractor;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string path = "C:\\Dev\\GitStractor";
+        string outputPath = Path.Combine(Environment.CurrentDirectory, "output.csv");
+        
+        GitStractor gitstractor = new();
+        gitstractor.ExtractCommitInformation(path, outputPath);
+    }
+}
---
+++ MattEland.GitStractor/MattEland.GitStractor/GitStractor.cs
@@ -0,0 +1,12 @@
+﻿namespace MattEland.GitStractor;
+
+public class GitStractor
+{
+    public void ExtractCommitInformation(string repoPath, string outputPath)
+    {
+        // Do something
+        LibGit2Sharp.Repository repo = new(repoPath);
+
+        repo.Commits.ToList().ForEach(c => Console.WriteLine(c.Message.Trim()));
+    }
+}
