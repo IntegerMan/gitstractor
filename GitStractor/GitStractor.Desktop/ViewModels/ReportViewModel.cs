d87f23cbb8142453b5fb2261b59acbf810ca3c58
Matt Eland <matt.eland@gmail.com>
2023-04-05 00:10:25 -0400

    A few working charts

--- GitStractor/GitStractor.Desktop/ViewModels/ReportViewModel.cs
+++ GitStractor/GitStractor.Desktop/ViewModels/ReportViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,8 +21,25 @@ public class ReportViewModel : ViewModelBase
 
         string commitPath = Path.Combine(csvFilePath, "Commits.csv");
 
-        _commits = CommitsCsvReader.ReadCommits(commitPath).ToList();
+        _commits = CommitsCsvReader.ReadCommits(commitPath).OrderBy(c => c.AuthorDateUTC).ToList();
     }
 
     public IEnumerable<CommitData> Commits => _commits;
+    public IEnumerable AuthorsOverTime
+    {
+        get
+        {
+            IEnumerable<IGrouping<DateTime, CommitData>> authorsOverTime = _commits.GroupBy(c => c.AuthorDateUTC.Date);
+
+            return authorsOverTime.Select(g =>
+                new
+                {
+                    Date = g.Key,
+                    Count = g.DistinctBy(c => c.AuthorEmail).Count()
+                });
+        }
+    }
+
+    public DateTime MaxDate => _commits.Max(c => c.AuthorDateUTC).AddMonths(1);
+    public DateTime MinDate => _commits.Min(c => c.AuthorDateUTC).AddMonths(-1);
 }
