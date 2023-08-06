d8c01e3fc0be0e687bd9db20cd17a60e4c5117bf
Matt Eland <matt.eland@gmail.com>
2023-08-05 20:18:46 -0400

    Better argument parsing

---
+++ GitStractor/GitStractor-Acquisition/GitStractorAcquireOptions.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace GitStractor.Acquire;
+
+public class GitStractorAcquireOptions
+{
+    [Option('r', "repository", Required = true, HelpText = "The path to the repository to clone. This should be a URL to a git repository.")]
+    public required string Repository { get; init; }
+
+    [Option('p', "path", Required = false, Default = null, HelpText = "The path to the output folder where the repository will be stored. The .git folder and all repo contents will be added directly to this folder. Defaults to the current directory.")]
+    public string? ExtractPath { get; set; }
+
+    [Option('o', "overwrite", Required = false, Default = false, HelpText = "If true, the output folder will be deleted if it exists before the repository is cloned. Otherwise, the clone will fail if the path exists.")]
+    public bool OverwriteIfExists { get; set; }
+}
--- GitStractor/GitStractor-Acquisition/Program.cs
+++ GitStractor/GitStractor-Acquisition/Program.cs
@@ -1,37 +1,47 @@
-﻿using LibGit2Sharp;
+﻿using CommandLine;
+using LibGit2Sharp;
 
-public class Program {
+namespace GitStractor.Acquire;
 
+public class Program
+{
     private const int SuccessCode = 0;
     private const int ValidationErrorCode = -1;
     private const int BadGitUrlErrorCode = -2;
     private const int FolderAlreadyExistsErrorCode = -2;
     private const int BadRepoErrorCode = -4;
-    private const int FolderIOErrorCode = -5;
+    private const int DiskWriteErrorCode = -5;
+    private const int UnknownErrorCode = -6;
 
-    public static int Main(string[] args) {
-        // Validate arguments
-        // TODO: There are C# libraries that can work with this in a very nice and object-oriented way
-        if (args == null || args.Length < 1 || args.Length > 2) {
-            Console.WriteLine($"Invalid usage called with arguments {string.Join(' ', args ?? Array.Empty<string>())}");
-            Console.WriteLine();
-            Console.WriteLine(@"Expected usage: 'Gitstractor-Acquire ""git_repo_url.git"" ""C:\Path\On\Disk""");
-
-            // TODO: Report to app insights
-
+    public static int Main(string[] args)
+    {
+        // Handle parsing and validation
+        ParserResult<GitStractorAcquireOptions> parserResult =
+            Parser.Default.ParseArguments<GitStractorAcquireOptions>(args);
+       
+        if (parserResult.Tag == ParserResultType.NotParsed)
+        {
             return ValidationErrorCode;
         }
 
         // Extract arguments
-        string gitRepo = args[0];
-        string clonePath = args.Length == 2 ? args[1] : Environment.CurrentDirectory;
+        GitStractorAcquireOptions options = parserResult.Value;
+        options.ExtractPath ??= Environment.CurrentDirectory;
 
         // TODO: Report to app insights
 
         // Attempt to clone the repository
-        try {
-            Console.WriteLine($"Cloning '{gitRepo}' to {clonePath}");
-            string resultingPath = Repository.Clone(gitRepo, clonePath);
+        try
+        {
+            // If the user specified they should delete the directory and it exists, delete it
+            if (options.OverwriteIfExists && Directory.Exists(options.ExtractPath))
+            {
+                Console.WriteLine($"Deleting existing directory {options.ExtractPath}");
+                Directory.Delete(options.ExtractPath, recursive: true);
+            }
+
+            Console.WriteLine($"Cloning '{options.Repository}' to {options.ExtractPath}");
+            string resultingPath = Repository.Clone(options.Repository, options.ExtractPath);
 
             Console.WriteLine($"Repository cloned to '{resultingPath}'");
 
@@ -43,39 +53,65 @@ public class Program {
 
             return SuccessCode;
         }
-        catch (RecurseSubmodulesException ex) {
+        catch (IOException ex)
+        {
+            Console.Error.WriteLine($"Failed to delete directory {options.ExtractPath}: {ex.Message}");
+            return DiskWriteErrorCode;
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.Error.WriteLine($"Insufficient permissions for directory {options.ExtractPath}: {ex.Message}");
+            return DiskWriteErrorCode;
+        }
+        catch (RecurseSubmodulesException ex)
+        {
             ReportException(ex, $"The repository has recursive submodules and cannot be cloned: {ex.Message}");
             return BadRepoErrorCode;
         }
-        catch (NameConflictException ex) {
+        catch (NameConflictException ex)
+        {
             ReportException(ex, ex.Message); // this exception's details are pretty helpful here
             return FolderAlreadyExistsErrorCode;
         }
-        catch (LibGit2SharpException ex) {
-            if (ex.Message.Contains("401")) {
-                ReportException(ex, $"The repository '{gitRepo}' could not be found and may be private. Check the URL and access settings.");
-                return BadGitUrlErrorCode;
-            } else if (ex.Message.Contains("unsupported", StringComparison.OrdinalIgnoreCase)) {
-                ReportException(ex, $"Could not clone repository '{gitRepo}': {ex.Message}");
-                return BadGitUrlErrorCode;
-            } else if (ex.Message.Contains("unexpected content-type", StringComparison.OrdinalIgnoreCase)) {
-                ReportException(ex, $"Could not find a git repository at '{gitRepo}'");
-                return BadGitUrlErrorCode;
-            } else if (ex.Message.Contains("failed to make directory ", StringComparison.OrdinalIgnoreCase)) {
-                ReportException(ex, $"Could not save the repository to disk: {ex.Message}");
-                return FolderIOErrorCode;
-            } else {
-                ReportException(ex, $"An unexpected error occurred cloning '{gitRepo}': {ex.GetType().Name} {ex.Message}");
-
-                // This is something that didn't come up in testing. Let's throw it and let the invoking process know about it
-                throw;
-            }
-
+        catch (LibGit2SharpException ex)
+        {
+            return HandleLibGit2SharpException(ex, options);
         }
-
     }
 
-    private static void ReportException(Exception ex, string userFacingMessage) {
+    private static int HandleLibGit2SharpException(LibGit2SharpException ex, GitStractorAcquireOptions options)
+    {
+        if (ex.Message.Contains("401"))
+        {
+            ReportException(ex,
+                $"The repository '{options.Repository}' could not be found and may be private. Check the URL and access settings.");
+            return BadGitUrlErrorCode;
+        }
+
+        if (ex.Message.Contains("unsupported", StringComparison.OrdinalIgnoreCase))
+        {
+            ReportException(ex, $"Could not clone repository '{options.Repository}': {ex.Message}");
+            return BadGitUrlErrorCode;
+        }
+
+        if (ex.Message.Contains("unexpected content-type", StringComparison.OrdinalIgnoreCase))
+        {
+            ReportException(ex, $"Could not find a git repository at '{options.Repository}'");
+            return BadGitUrlErrorCode;
+        }
+
+        if (ex.Message.Contains("failed to make directory", StringComparison.OrdinalIgnoreCase))
+        {
+            ReportException(ex, $"Could not save the repository to disk: {ex.Message}");
+            return DiskWriteErrorCode;
+        }
+
+        ReportException(ex, $"An unexpected error occurred cloning '{options.Repository}': {ex.GetType().Name} {ex.Message}");
+        return UnknownErrorCode;
+    }
+
+    private static void ReportException(Exception ex, string userFacingMessage)
+    {
         Console.WriteLine(userFacingMessage);
 
         // TODO: Report to app insights
