f227e23487b13fdfde78cfb11fec8d7537cd1ed1
Matt Eland <matt.eland@gmail.com>
2023-08-05 20:29:36 -0400

    Restructured into classes

--- GitStractor/GitStractor-Acquisition/Program.cs
+++ GitStractor/GitStractor-Acquisition/Program.cs
@@ -1,18 +1,9 @@
 ﻿using CommandLine;
-using LibGit2Sharp;
 
 namespace GitStractor.Acquire;
 
 public class Program
 {
-    private const int SuccessCode = 0;
-    private const int ValidationErrorCode = -1;
-    private const int BadGitUrlErrorCode = -2;
-    private const int FolderAlreadyExistsErrorCode = -2;
-    private const int BadRepoErrorCode = -4;
-    private const int DiskWriteErrorCode = -5;
-    private const int UnknownErrorCode = -6;
-
     public static int Main(string[] args)
     {
         // Handle parsing and validation
@@ -21,100 +12,18 @@ public class Program
        
         if (parserResult.Tag == ParserResultType.NotParsed)
         {
-            return ValidationErrorCode;
+            return (int) CloneResult.ValidationError;
         }
 
         // Extract arguments
         GitStractorAcquireOptions options = parserResult.Value;
-        options.ExtractPath ??= Environment.CurrentDirectory;
 
         // TODO: Report to app insights
 
         // Attempt to clone the repository
-        try
-        {
-            // If the user specified they should delete the directory and it exists, delete it
-            if (options.OverwriteIfExists && Directory.Exists(options.ExtractPath))
-            {
-                Console.WriteLine($"Deleting existing directory {options.ExtractPath}");
-                Directory.Delete(options.ExtractPath, recursive: true);
-            }
+        RepositoryCloner cloner = new();
+        CloneResult result = cloner.Clone(options.Repository, options.ExtractPath, options.OverwriteIfExists);
 
-            Console.WriteLine($"Cloning '{options.Repository}' to {options.ExtractPath}");
-            string resultingPath = Repository.Clone(options.Repository, options.ExtractPath);
-
-            Console.WriteLine($"Repository cloned to '{resultingPath}'");
-
-            // TODO: Report to app insights
-
-            // TODO: Enqueue processing of this repository on RabbitMQ or an Azure Queue
-
-            // TODO: Report to app insights
-
-            return SuccessCode;
-        }
-        catch (IOException ex)
-        {
-            Console.Error.WriteLine($"Failed to delete directory {options.ExtractPath}: {ex.Message}");
-            return DiskWriteErrorCode;
-        }
-        catch (UnauthorizedAccessException ex)
-        {
-            Console.Error.WriteLine($"Insufficient permissions for directory {options.ExtractPath}: {ex.Message}");
-            return DiskWriteErrorCode;
-        }
-        catch (RecurseSubmodulesException ex)
-        {
-            ReportException(ex, $"The repository has recursive submodules and cannot be cloned: {ex.Message}");
-            return BadRepoErrorCode;
-        }
-        catch (NameConflictException ex)
-        {
-            ReportException(ex, ex.Message); // this exception's details are pretty helpful here
-            return FolderAlreadyExistsErrorCode;
-        }
-        catch (LibGit2SharpException ex)
-        {
-            return HandleLibGit2SharpException(ex, options);
-        }
+        return (int) result;
     }
-
-    private static int HandleLibGit2SharpException(LibGit2SharpException ex, GitStractorAcquireOptions options)
-    {
-        if (ex.Message.Contains("401"))
-        {
-            ReportException(ex,
-                $"The repository '{options.Repository}' could not be found and may be private. Check the URL and access settings.");
-            return BadGitUrlErrorCode;
-        }
-
-        if (ex.Message.Contains("unsupported", StringComparison.OrdinalIgnoreCase))
-        {
-            ReportException(ex, $"Could not clone repository '{options.Repository}': {ex.Message}");
-            return BadGitUrlErrorCode;
-        }
-
-        if (ex.Message.Contains("unexpected content-type", StringComparison.OrdinalIgnoreCase))
-        {
-            ReportException(ex, $"Could not find a git repository at '{options.Repository}'");
-            return BadGitUrlErrorCode;
-        }
-
-        if (ex.Message.Contains("failed to make directory", StringComparison.OrdinalIgnoreCase))
-        {
-            ReportException(ex, $"Could not save the repository to disk: {ex.Message}");
-            return DiskWriteErrorCode;
-        }
-
-        ReportException(ex, $"An unexpected error occurred cloning '{options.Repository}': {ex.GetType().Name} {ex.Message}");
-        return UnknownErrorCode;
-    }
-
-    private static void ReportException(Exception ex, string userFacingMessage)
-    {
-        Console.WriteLine(userFacingMessage);
-
-        // TODO: Report to app insights
-    }
-
 }
---
+++ GitStractor/GitStractor-Acquisition/RepositoryCloner.cs
@@ -0,0 +1,96 @@
+﻿using LibGit2Sharp;
+
+namespace GitStractor.Acquire;
+
+public class RepositoryCloner
+{
+    public CloneResult Clone(string repository, string? extractPath, bool overwriteIfExists = false)
+    {
+        extractPath ??= Environment.CurrentDirectory;
+
+        try
+        {
+            // If the user specified they should delete the directory and it exists, delete it
+            if (overwriteIfExists && Directory.Exists(extractPath))
+            {
+                Console.WriteLine($"Deleting existing directory {extractPath}");
+                Directory.Delete(extractPath, recursive: true);
+            }
+
+            Console.WriteLine($"Cloning '{repository}' to {extractPath}");
+            string resultingPath = Repository.Clone(repository, extractPath);
+
+            Console.WriteLine($"Repository cloned to '{resultingPath}'");
+
+            // TODO: Report to app insights
+
+            // TODO: Enqueue processing of this repository on RabbitMQ or an Azure Queue
+
+            // TODO: Report to app insights
+
+            return CloneResult.Success;
+        }
+        catch (IOException ex)
+        {
+            Console.Error.WriteLine($"Failed to delete directory {extractPath}: {ex.Message}");
+            return CloneResult.DiskWriteError;
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.Error.WriteLine($"Insufficient permissions for directory {extractPath}: {ex.Message}");
+            return CloneResult.DiskWriteError;
+        }
+        catch (RecurseSubmodulesException ex)
+        {
+            ReportException(ex, $"The repository has recursive submodules and cannot be cloned: {ex.Message}");
+            return CloneResult.BadRepository;
+        }
+        catch (NameConflictException ex)
+        {
+            ReportException(ex, ex.Message); // this exception's details are pretty helpful here
+            return CloneResult.FolderAlreadyExists;
+        }
+        catch (LibGit2SharpException ex)
+        {
+            return HandleLibGit2SharpException(ex, repository);
+        }
+    }
+
+    private CloneResult HandleLibGit2SharpException(Exception ex, string repository)
+    {
+        if (ex.Message.Contains("401"))
+        {
+            ReportException(ex,
+                $"The repository '{repository}' could not be found and may be private. Check the URL and access settings.");
+            return CloneResult.BadGitUrl;
+        }
+
+        if (ex.Message.Contains("unsupported", StringComparison.OrdinalIgnoreCase))
+        {
+            ReportException(ex, $"Could not clone repository '{repository}': {ex.Message}");
+            return CloneResult.BadGitUrl;
+        }
+
+        if (ex.Message.Contains("unexpected content-type", StringComparison.OrdinalIgnoreCase))
+        {
+            ReportException(ex, $"Could not find a git repository at '{repository}'");
+            return CloneResult.BadGitUrl;
+        }
+
+        if (ex.Message.Contains("failed to make directory", StringComparison.OrdinalIgnoreCase))
+        {
+            ReportException(ex, $"Could not save the repository to disk: {ex.Message}");
+            return CloneResult.DiskWriteError;
+        }
+
+        ReportException(ex, $"An unexpected error occurred cloning '{repository}': {ex.GetType().Name} {ex.Message}");
+        return CloneResult.UnknownError;
+    }
+
+    private void ReportException(Exception ex, string userFacingMessage)
+    {
+        Console.WriteLine(userFacingMessage);
+
+        // TODO: Report to app insights
+    }
+}
