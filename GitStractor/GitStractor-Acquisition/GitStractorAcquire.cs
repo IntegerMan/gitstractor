28f8611afa02bd347951937c8b6d9452836d0e62
Matt Eland <matt.eland@gmail.com>
2023-08-06 16:19:47 -0400

    Simplified the logic needed for an individual worker program

---
+++ GitStractor/GitStractor-Acquisition/GitStractorAcquire.cs
@@ -0,0 +1,34 @@
+﻿using GitStractor.Cloning;
+using GitStractor.Workers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GitStractor.Acquire;
+
+public class GitStractorAcquire : GitStractorProgram {
+    public override string UsageHelp => @"Usage: GitStractor-Acquire -r https://GitHub.com/IntegerMan/GitStractor -p C:\dev\GitStractor";
+
+    protected override IHostBuilder ConfigureHostBuilder(IHostBuilder builder, string[] args) =>
+        builder.UseConsoleLifetime()
+            .ConfigureServices((context, services) => {
+                services.AddTransient<RepositoryCloner>();
+
+                services.AddOptions<GitStractorAcquireOptions>()
+                        .Configure(options => context.Configuration.GetSection("Acquire").Bind(options))
+                        .ValidateDataAnnotations();
+
+                // Register our service
+                services.AddHostedService<GitStractorAcquisitionWorker>();
+            })
+            .ConfigureAppConfiguration(services => {
+                services.AddCommandLine(args, new Dictionary<string, string>() {
+                    { "-r", "Acquire:Repository" },
+                    { "--repository", "Acquire:Repository" },
+                    { "-p", "Acquire:ExtractPath" },
+                    { "--path", "Acquire:ExtractPath" },
+                    { "-o", "Acquire:OverwriteIfExists" },
+                    { "--overwrite", "Acquire:OverwriteIfExists" },
+                });
+            });
+}
--- GitStractor/GitStractor-Acquisition/Program.cs
+++ GitStractor/GitStractor-Acquisition/Program.cs
@@ -1,82 +1,9 @@
-﻿using GitStractor.Cloning;
-using GitStractor.Workers;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+﻿namespace GitStractor.Acquire;
 
-namespace GitStractor.Acquire;
-
-public class Program {
+public class Program  {
 
     public static int Main(string[] args) {
-        IHostBuilder builder = CreateHostBuilder(args);
-
-        try {
-            using GitStractorHost host = (GitStractorHost)builder.Build();
-            host.Run();
-
-            return host.Succeeded ? 0 : -1;
-        }
-        catch (OptionsValidationException ex) {
-            // Validate the provided configuration options
-            WriteErrorToConsole(ex.Message);
-            WriteHelpToConsole(@"Usage: GitStractor-Acquire -r https://GitHub.com/IntegerMan/GitStractor -p C:\dev\GitStractor");
-            return -2;
-        }
-        catch (AggregateException ex) {
-            // This typically happens when a worker encounters an exception shutting down
-            WriteErrorToConsole(ex.Message);
-
-            return -3;
-        }
+        GitStractorAcquire program = new();
+        return program.Run(args);
     }
-
-    private static void WriteErrorToConsole(string message) {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine();
-        Console.Error.WriteLine(message);
-        Console.WriteLine();
-        Console.ResetColor();
-    }
-
-    private static void WriteHelpToConsole(string message) {
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.WriteLine(message);
-        Console.ResetColor();
-    }
-
-
-    private static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .UseConsoleLifetime()
-            .ConfigureServices((context, services) => {
-                services.AddSingleton<IHost, GitStractorHost>();
-                services.AddTransient<RepositoryCloner>();
-
-                services.AddOptions<GitStractorHostOptions>()
-                        .Configure(options => context.Configuration.GetSection("GitStractorHost").Bind(options));
-                services.AddOptions<GitStractorAcquireOptions>()
-                        .Configure(options => context.Configuration.GetSection("Acquire").Bind(options))
-                        .ValidateDataAnnotations();
-
-                // Register our service
-                services.AddHostedService<GitStractorAcquisitionWorker>();
-            })
-            .ConfigureAppConfiguration(services => {
-                services.AddEnvironmentVariables(prefix: "GITSTRACTOR_");
-                services.AddCommandLine(args, new Dictionary<string, string>() {
-                    { "-r", "Acquire:Repository" },
-                    { "--repository", "Acquire:Repository" },
-                    { "-p", "Acquire:ExtractPath" },
-                    { "--path", "Acquire:ExtractPath" },
-                    { "-o", "Acquire:OverwriteIfExists" },
-                    { "--overwrite", "Acquire:OverwriteIfExists" },
-                });
-            })
-            .ConfigureLogging((hostContext, config) => {
-                config.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
-                config.AddConsole();
-            });
 }
